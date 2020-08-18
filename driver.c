c5c9b62d3b022f644b4113a379331da2d0fcd1a8
smaccary <=>
2020-08-18 18:29:31 +0200

    added more strlen test

--- driver.c
+++ driver.c
@@ -6,13 +6,17 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 14:44:48 by user42            #+#    #+#             */
-/*   Updated: 2020/08/18 18:12:09 by user42           ###   ########.fr       */
+/*   Updated: 2020/08/18 18:28:34 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #define BLACK	 	"\e[30m"
 #define RED 		"\e[31m"
@@ -25,6 +29,7 @@
 #define RESET 		"\e[0m"
 
 #define SUCCESS_CODE 0
+#define READ_SIZE	4096
 
 #define TEST(test, ...) (test(__VA_ARGS__) == SUCCESS_CODE) ? printf("%s%s (l:%d): [%s]\n%s", GREEN, #test, __LINE__, "OK", RESET) : printf("%s%s (l:%d): [%s]\nARGS : " #__VA_ARGS__ "\n%s", RED, #test, __LINE__, "KO", RESET)
 
@@ -35,25 +40,46 @@ char    *strings[] = {"ABCDEF", "Hello World!", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 int get_n();
 size_t	ft_strlen(const char *str);
 
+int	strlen_cmp(char *arg)
+{
+	int		real_ret;
+	int		ft_ret;
+	
+	real_ret = strlen(arg);
+	ft_ret = ft_strlen(arg);
+	if (real_ret != ft_ret)
+	{
+		printf(RED "[STRLEN] ERROR : [%s]\nSTRLEN: %d\nFT_STRLEN: %d\n" RESET,
+				arg, real_ret, ft_ret);
+		return (1);
+	}
+	return (0);
+}
+
 int strlen_test(char **split)
 {
 	int 	i;
-	int		real_ret;
-	int		ft_ret;
+	char	ascii[CHAR_MAX + 1];
+	char	rd_buff[READ_SIZE + 1];
+	int		fd;
+	int 	err;
 
 	i = -1;
+	err = 0;
 	while (split[++i])
+		err |= strlen_cmp(split[i]);
+	i = -1;
+	while (++i < CHAR_MAX)
+		ascii[i] = i + 1;
+	ascii[i] = 0;
+	err |= strlen_cmp(ascii);
+	fd = open("random_text", O_RDONLY);
+	while (read(fd, rd_buff, READ_SIZE) > 0)
 	{
-		real_ret = strlen(strings[i]);
-		ft_ret = ft_strlen(strings[i]);
-		if (real_ret != ft_ret)
-		{
-			printf(RED "[STRLEN] ERROR : [%s]\nSTRLEN: %d\nFT_STRLEN: %d\n" RESET,
-					split[i], real_ret, ft_ret);
-			return (1);
-		}
+		rd_buff[READ_SIZE] = 0;
+		err |= strlen_cmp(rd_buff);
 	}
-	return (0);
+	return (err);
 }
 
 int main(void)
