85579be5eff691ef422f04555218f9fe42697c38
smaccary <=>
2020-08-19 16:16:53 +0200

    comment ft_strcpy.s

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,17 +1,16 @@
 global ft_strcpy
 
 ft_strcpy:
-	mov		r9,rdi
-	xor     rdx, rdx   ; i = 0
+	xor     rdx, rdx;	i = 0
 
 loop:
 	mov	eax, [rsi + rdx];	tmp = src[i]
 	mov [rdi + rdx], eax; 	dst[i] = tmp
 	cmp byte [rsi + rdx], byte 0; if src[i] == 0
-	je end  ; then  | jump to end
-	inc rdx ; else  | i++
-	jmp loop ;      | jump to loop beginning
+	je end; 						|then	jump to end
+	inc rdx; 						|else	| i++
+	jmp loop;						|		| jump to loop
 
 end:
-	mov rax,r9
-	ret ; return (dst)
+	mov rax,rdi;	return_value = dst
+	ret; 			return (return_value)
